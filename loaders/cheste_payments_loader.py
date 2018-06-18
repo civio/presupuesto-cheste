d6e10f811ba695ba1e6a90e58e80b4d25334abe4
Eduardo Sebastian <esebastian@altiora.es>
2018-06-18 14:08:02 +0200

    Configure payments loader

--- loaders/cheste_payments_loader.py
+++ loaders/cheste_payments_loader.py
@@ -1,26 +1,65 @@
 # -*- coding: UTF-8 -*-
-import datetime
-
 from budget_app.loaders import PaymentsLoader
 from budget_app.models import Budget
 
-class ChestePaymentsLoader(PaymentsLoader):
 
+payments_mapping = {
+    'default': {'fc_code': 2, 'date': 5, 'payee': 7, 'description': 8, 'amount': 9},
+}
+
+
+class PaymentsCsvMapper:
+    def __init__(self, year):
+        mapping = payments_mapping.get(str(year))
+
+        if not mapping:
+            mapping = payments_mapping.get('default')
+
+        self.fc_code = mapping.get('fc_code')
+        self.date = mapping.get('date')
+        self.payee = mapping.get('payee')
+        self.description = mapping.get('description')
+        self.amount = mapping.get('amount')
+
+
+class ChestePaymentsLoader(PaymentsLoader):
     # Parse an input line into fields
     def parse_item(self, budget, line):
+        # Mapper
+        mapper = PaymentsCsvMapper(budget.year)
 
-        policy_id = line[1].strip()[:2] # First two digits of the programme make the policy id
+        # First two digits of the programme make the policy id
+        policy_id = line[mapper.fc_code].strip()[:2]
+
         # But what we want as area is the policy description
         policy = Budget.objects.get_all_descriptions(budget.entity)['functional'][policy_id]
+
+        # We got an iso date
+        date = line[mapper.date]
+
+        # Payee data
+        payee = line[mapper.payee].strip()
+
+        # We got some anonymized entries
+        anonymized = False
+        anonymized = (True if payee == 'ANONIMIZADO' else anonymized)
+
+        # Description
+        description = line[mapper.description].strip()
+
+        # Amount
+        amount = line[mapper.amount]
+        amount = self._read_english_number(amount)
 
         return {
             'area': policy,
             'programme': None,
-            'fc_code': None,  # We don't try (yet) to have foreign keys to existing records
+            'ic_code': None,
+            'fc_code': None,
             'ec_code': None,
-            'date': line[3].strip(),
-            'payee': self._titlecase(line[5].strip()),
-            'anonymized': False,
-            'description': line[6].strip(),
-            'amount': self._read_english_number(line[7])
+            'date': date,
+            'payee': payee,
+            'anonymized': anonymized,
+            'description': description,
+            'amount': amount
         }
